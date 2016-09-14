f27542c169f76441c92e1c2c8565377302e09610
eileenrob84 <eileenrob84@gmail.com>
2016-09-13 20:11:35 -0500

    Add files via upload

---
+++ Cindiv.java
@@ -0,0 +1,18 @@
+
+public class Cindiv (id) {
+	
+	String Id;
+	String Name;
+	String Sex;
+	Cdate birth;
+	Boolean alive;
+	Cdate death;
+	String famc;  //family id?
+	list<String> fams;
+	
+	// Set and Access methods including Addfams()
+	
+	// Print format methods?
+
+
+}
