f20c2887be4d1d5a2542a26827e8cfffabae7e9e
eileenrob84 <eileenrob84@gmail.com>
2016-10-01 21:59:24 -0500

    Update Functions.java
    update for User Story 13

--- gedcomparser/src/Functions.java
+++ gedcomparser/src/Functions.java
@@ -410,7 +410,53 @@ public class Functions {
 	}
 	// end get number of children	
 
+		//********************************************************************
+	// User Story 13
+	// Print anomaly if more 2 children have birthdates that are more than
+	// 2 days apart but less than 8 months
+	//********************************************************************
 	
+	private static void checkIfSibsNotTooClose(IndivContainer indivContainer, 
+			FamilyContainer familyContainer){
+
+		CFamily fam;
+
+		// for each family
+		for (int num=0; num < familyContainer.getSize(); num++)
+		{
+
+		// if number of children > 1
+		     if (fam.getNumberOfChildren() > 1)
+		     {
+			// for each child 
+		           for(int i = 0; i < fam.getNumberOfChildren() - 1; i++)
+		          {
+		            Cindiv child1;
+
+		             // Get child1
+		             child1 = indivContainer.findIndiv(fam.getChildID(i));
+
+		              // for each additional child
+		              for (int j = i+1; j < fam.getNumberOfChildren(); j++)
+		              {
+		            	  Cindiv child2;
+		            	  
+		                 // Get child2
+		                   child2 = indivContainer.findIndiv(fam.getChildID(j));
+
+		                   // if date is within 8 months and not within 2 days
+		                   If ((child1.getBirthDate().isWithin(child2.getBirthDate(), 0, 8, 0)) &&
+		                       (!child1.getBirthDate().isWithin(child2.getBirthDate(), 0, 0, 2))
+			                {
+					   String anom = new String(child1 + " and " + child2 + "are not likely "
+		                            		+ "to be siblings")
+				           printError(true, "US13", anom);
+		                    } // if
+		               } // for j
+		           } // for i
+		        } // if children > 1
+		    } //for each family
+		} // function
 	
 	
 	//********************************************************************
