8c159cf9da6b08ecf85705ee3e0a3ce116e7c2ce
eileenrob84 <eileenrob84@gmail.com>
2016-10-16 18:56:22 -0500

    updates for US11

--- gedcomparser/src/Functions.java
+++ gedcomparser/src/Functions.java
@@ -1,3 +1,5 @@
+package gedcom;
+
 // Class is a collection of functions to parse, print, and validate/error check
 // a GEDCOM file.
 // NOTE: these were originally contained in gedcomparser Class but have been 
@@ -16,8 +18,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-
-import gedcom.*;
 
 public class Functions {
 	
@@ -293,7 +293,7 @@ public class Functions {
 			
 					if(isSameNameBDate(indiv, indivForCompare)){						
 						String errorString = "Individual " + indiv.getId() + " " + indiv.getName() + 
-								" with birthdate " +indiv.getDateBirth().getStringDate() + " is not unique in this GEDCOM file.\n"
+								" with birthdate " +indiv.getDateBirth()+ " is not unique in this GEDCOM file.\n"
 								+ "Duplicate individual ID is " + indivForCompare.getId() + ".";
 						duplicates.add(indivForCompare);
 				
@@ -316,8 +316,7 @@ public class Functions {
 	// duplicate individuals
 	//********************************************************************	
 	public static boolean isSameNameBDate(Cindiv i1, Cindiv i2){
-		return i1.getName().equals(i2.getName()) && 
-				(i1.getDateBirth().getStringDate().equals(i2.getDateBirth().getStringDate()));
+		return i1.getName().equals(i2.getName()) && (i1.getDateBirth().equals(i2.getDateBirth()));
 	}
 	
 	//********************************************************************
@@ -356,8 +355,7 @@ public class Functions {
 			indiv = indivContainer.getIndiv(num);
 			if (indiv.isDeathBeforeBirth()) {
 				String text = String.format("Death date (%s) of %s (%s) occurs before birth date (%s)", 
-						indiv.getDateDeath().getStringDate(), indiv.getName(), indiv.getId(), 
-						indiv.getDateBirth().getStringDate());
+						indiv.getDateDeath(), indiv.getName(), indiv.getId(), indiv.getDateBirth());
 				printError(true, "US03", text);
 			}
 		}
@@ -417,8 +415,8 @@ public class Functions {
 		                child2 = indivContainer.findIndiv(fam.getChildID(j));
 
 		                // if date is within 8 months and not within 2 days
-		                if ((child1.getDateBirth().isWithin(child2.getDateBirth(), 0, 8, 0)) &&
-		                       (!child1.getDateBirth().isWithin(child2.getDateBirth(), 0, 0, 2)))
+		                if ((child1.getBirthDate().isWithin(child2.getBirthDate(), 0, 8, 0)) &&
+		                       (!child1.getBirthDate().isWithin(child2.getBirthDate(), 0, 0, 2)))
 			            {
 		                	String anom = new String(child1.getId() + " and " + child2.getId() + " are not likely "
 		                            		+ "to be siblings in family: " + fam.getFamID());
@@ -525,8 +523,8 @@ public class Functions {
 			}
 			if (count == 0) System.out.println("None");
 		}
-	
-			//********************************************************************
+		
+		//********************************************************************
 		//   
 		//   Marriage should occur before death
 		//   US_ID  05
@@ -560,7 +558,55 @@ public class Functions {
 			}// for loop
 			
 		 }// end marriageBeforeDeath
-	
+		
+		//********************************************************************
+		//   
+		//   No bigamy
+		//   US_ID  11
+		//   
+		//********************************************************************
+		
+		public static Boolean wasMarried(CFamily fam, Cdate dt)
+		{
+			Boolean wasMar = true;
+			if (fam.getMarriedDate().isAfter(dt)) wasMar = false;
+			if ((fam.isDivorced()) && dt.isAfter(fam.getDivorcedDate())) wasMar = false;
+			Cindiv husb = indivContainer.findIndiv(fam.getHusbandID());
+			Cindiv wife = indivContainer.findIndiv(fam.getWifeID());
+			if (!husb.getIsAlive() && dt.isAfter(husb.getDateDeath())) wasMar = false;
+			if (!wife.getIsAlive() && dt.isAfter(wife.getDateDeath())) wasMar = false;
+			
+			return wasMar;
+		}
+		
+		public static void checkForBigamy()
+		{
+		    for (int num = 0; num < indivContainer.getSize(); num++)
+		    {
+		        Cindiv indiv = indivContainer.getIndiv(num);
+		        if (indiv.getFamS().size() > 1)
+		        {
+		            for (int famx=0; famx < (indiv.getFamS().size() - 1); famx++)
+		            {
+		                for (int cmpx=famx+1; cmpx < indiv.getFamS().size(); cmpx++)
+		                {
+		                    CFamily fam = familyContainer.findFam(indiv.getFamS().get(famx));
+		                    CFamily cmp = familyContainer.findFam(indiv.getFamS().get(cmpx));
+		                    if (wasMarried(fam, cmp.getMarriedDate()) || wasMarried(cmp, fam.getMarriedDate()))
+		                    {
+		                        String anom = new String ("individual " + indiv.getId() + " is married to more than one person");
+		                        printError(true, "US11", anom);
+		                    }
+		                }
+		            }
+		        }
+		    }
+					
+		}// end of checkForBigamy
+				
+				
+				
+				
 	
 	// Print section****************************************************************************************	
 	//********************************************************************
@@ -578,10 +624,10 @@ public class Functions {
 			System.out.printf("%-20s%s\n", "ID:", indiv.getId());
 			System.out.printf("%-20s%s\n", "Name", indiv.getName());
 			System.out.printf("%-20s%s\n", "Gender:", indiv.getGender());
-			System.out.printf("%-20s%s\n", "Date of Birth:", indiv.getDateBirth().getStringDate());
+			System.out.printf("%-20s%s\n", "Date of Birth:", indiv.getDateBirth());
 			System.out.printf("%-20s%s\n", "Alive:", indiv.getIsAlive());
 			if(!indiv.getIsAlive())
-				System.out.printf("%-20s%s\n", "Date of Death:", indiv.getDateDeath().getStringDate());
+				System.out.printf("%-20s%s\n", "Date of Death:", indiv.getDateDeath());
 			System.out.printf("%-20s%s\n", "Child of Family:", indiv.getFamC());
 			System.out.printf("%-20s%s\n", "Spouse of Family:", indiv.getFamS());
 			
@@ -641,50 +687,4 @@ public class Functions {
 		System.out.printf("\n%s: %s: %s\n", msg, US_ID, text);
 			
 	}
-	
-	// MISC **************************************************************
-	//********************************************************************
-	// Print descendants and spouses of those who've died in the last 30 
-	// days. NOTE: this currently prints as an anomaly.
-	// US37
-	//********************************************************************	
-	public static void listSurvivors(){
-		ArrayList<Cindiv> died = findRecentDied();
-		ArrayList<Cindiv> descendants = new ArrayList<Cindiv>();
-
-		for(int i = 0; i<died.size(); i++){
-			for(int j = 0; j<indivContainer.getSize(); j++){
-				if(isDescendantOf(died.get(i).getId(), indivContainer.getIndiv(j).getId())){
-					descendants.add(indivContainer.getIndiv(j));
-				}
-			}
-			
-			if(descendants.size()>0){
-				String errorString = "Recently deceased individual "+died.get(i).getId()+" has the following descendants:\n";
-				for(int j = 0; j<descendants.size(); j++){
-					errorString += descendants.get(j).getId();
-					if(j<descendants.size()-1){
-						errorString += "\n";
-					}
-				}				
-			
-				printError(false, "US37", errorString);		
-			}			
-		}		
-	}
-
-	//********************************************************************
-	// @returns ArrayList of individuals who've died in last 30 days
-	//********************************************************************	
-	public static ArrayList<Cindiv> findRecentDied(){
-		Cdate today = new Cdate();
-		ArrayList<Cindiv> dec = new ArrayList<Cindiv>();
-		
-		for(int i = 0; i<indivContainer.getSize(); i++){		
-			if(!indivContainer.getIndiv(i).getIsAlive() && indivContainer.getIndiv(i).getDateDeath().isWithin(today, 0, 0, 30)){
-				dec.add(indivContainer.getIndiv(i));
-			}
-		}		
-		return dec;
-	}	
 }
